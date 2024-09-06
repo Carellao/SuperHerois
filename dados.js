b8b7d5aa36861e4979a07b00b0640936d10e70b1
Carellao <165285623+Carellao@users.noreply.github.com>
2024-09-06 16:32:16 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "Superman",
+        descricao: "Nascido no planeta Krypton, Kal-El foi enviado em uma nave espacial por seus pais, Jor-El e Lara, momentos antes da destruição de seu planeta natal. A nave espacial pousou em Smallville, Kansas, onde foi encontrado e criado por um casal de fazendeiros, Jonathan e Martha Kent.",
+        link: "https://pt.wikipedia.org/wiki/Superman",
+        tags: "força, laser, kriptonita"
+    },
+    {
+        titulo: "Batman",
+        descricao: "Bruce Wayne, órfão após o assassinato de seus pais, treinou seu corpo e mente para combater o crime em Gotham City.Equipado com um arsenal de gadgets e tecnologia de ponta, ele se tornou o vigilante conhecido como Batman.",
+        link: "https://pt.wikipedia.org/wiki/Batman",
+        tags: "morcego, escuro, rico"
+    },
+    {
+        titulo: "Lanterna Verde",
+        descricao: "Um membro do Corpo dos Lanternas Verdes, escolhido por sua força de vontade. Ele utiliza um anel de poder para criar construções de energia verde, baseado em seus pensamentos.",
+        link: "https://pt.wikipedia.org/wiki/Lanterna_Verde",
+        tags: "anel, avião, verde"
+    },
+    {
+        titulo: "Mulher Maravilha",
+        descricao: "Princesa das Amazonas, Diana de Temiscira, foi moldada em argila e dada vida pelos deuses. Criada em uma ilha paradisíaca, ela possui força sobre-humana, agilidade e um laço da verdade indestrutível.",
+        link: "https://pt.wikipedia.org/wiki/Mulher_Maravilha",
+        tags: "atena, amazonas, linda"
+    },
+    {
+        titulo: "Aquaman",
+        descricao: "Arthur Curry, meio humano e meio atlante, é o rei de Atlântida e protetor dos oceanos. Possui a capacidade de se comunicar com os animais marinhos e possui força sobre-humana debaixo d'água.",
+        link: "https://pt.wikipedia.org/wiki/Aquaman",
+        tags: "água, peixe, tridente"
+    },
+    {
+        titulo: "The Flash",
+        descricao: "Barry Allen, um cientista forense, foi atingido por um raio durante uma tempestade elétrica e banhado por produtos químicos. Desperto com a habilidade de se mover em velocidades cósmicas, ele se tornou o Flash, o homem mais rápido do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Flash_(DC_Comics)",
+        tags: "rápido, raio, amor"
+    },
+];
